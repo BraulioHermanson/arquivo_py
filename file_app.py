b7bfe8fa0b560c8cc4ae284592db59030fefdbb0
BraulioHermanson <brauliohcampos0@outlook.com>
2023-09-13 11:51:51 -0300

    commit

--- file_app.py
+++ file_app.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 compras = {}
 
@@ -26,10 +27,42 @@ def carregar_compras(nome_arquivo):
         return json.load(arquivo)
 
 def gerenciar_compras(compras):
-    pass
+    while True:
+        print("1 Adicionar item")
+        print("2 Remover item")
+        print("3 Visualizar lista")
+        print("4 Salvar e sair ")
+        print("5 Sair sem salvar")
+        escolha = input("Escolha uma opção: ")
+
+        if escolha == "1":
+            pass
+        elif escolha == "2":
+            pass
+        elif escolha == "3":
+            pass
+        elif escolha == "4":
+            pass
+        elif escolha == "5":
+            break
+        else:
+            print("Opção inválida")
+            time.sleep(1)
 
 def main():
     while True:
         print("1 Criar uma nova lista de compras")
         print("2 Carregar uma lista existente")
         print("3 Sair")
+        escolha = input("Esoolha uma opção: ")
+        if escolha == "1":
+            pass
+        elif escolha == "2":
+            pass
+        elif escolha == "3":
+            break
+        else:
+            print("Opção inválida")
+            time.sleep(1)
+
+main()
